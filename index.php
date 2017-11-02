6f1a6adb3db51f861bd7ce32e06e85d589981b1e
camillabinne <camillabinne@gmail.com>
2017-11-02 22:53:49 +0100

    hej

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <!-- Denne gør mobilvenlig/ respontive)´-->
-  <title>ROOM8 | Kontakt</title>
+  <title>ROOM8 | Kontakt </title>
   
   <link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
